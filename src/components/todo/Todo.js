4ced03532d9ac46689f9c9447d4acbbfb7542d84
gamzenurtukel <gamzenurtukel@gmail.com>
2022-11-26 17:25:49 +0300

    TodoForm import

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,10 +1,13 @@
 import "./Todo.scss";
 
+import TodoForm from "../todoForm";
+
 function Todo() {
   return (
     <div className="todo-container">
       <div className="todo-item">
         <h3>Daily To Do List</h3>
+        <TodoForm />
       </div>
     </div>
   );
