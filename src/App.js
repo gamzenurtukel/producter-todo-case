6ad84c2bc48aa87319ab7e6181aad206917e7ad8
gamzenurtukel <gamzenurtukel@gmail.com>
2022-11-26 15:47:27 +0300

    todo component created and color.scss changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.scss";
+import Todo from "./components/todo";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/todo/Todo.js
@@ -0,0 +1,13 @@
+import "./Todo.scss";
+
+function Todo() {
+  return (
+    <div className="todo-container">
+      <div className="todo-item">
+        <h3>Daily To Do List</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
